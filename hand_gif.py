2c99b1d8884362d8d91f566b8519f1949868a914
uyenbhku <uyen.bhku@gmail.com>
2023-06-15 22:35:10 +0700

    modify hand

--- hand_gif.py
+++ hand_gif.py
@@ -35,6 +35,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         success, frame = cap.read()
         if not success:
             break
+        frame = cv2.rotate(frame, cv2.ROTATE_180)
         frame = cv2.resize(frame, (1280, 720))
         # to improve performance, mark the image as not writeable to pass by reference instead of making a copy
         frame.flags.writeable = False
@@ -47,11 +48,13 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         fr_w, fr_h, fr_c = frame.shape
         # blending variable
         alpha = 0
-        beta = 0.1
+        beta = 0.05
         gamma = 0.6
 
         if results.left_hand_landmarks and results.right_hand_landmarks:
-            if abs(results.left_hand_landmarks.landmark[3].x - results.left_hand_landmarks.landmark[6].x) < 0.03 and abs(results.left_hand_landmarks.landmark[3].y - results.left_hand_landmarks.landmark[6].y) < 0.03 and abs(results.right_hand_landmarks.landmark[3].x - results.right_hand_landmarks.landmark[6].x) < 0.03 and abs(results.right_hand_landmarks.landmark[3].y - results.right_hand_landmarks.landmark[6].y) < 0.03:
+            right_count_1 = 0
+            left_count_1 = 0
+            if abs(results.left_hand_landmarks.landmark[3].x - results.left_hand_landmarks.landmark[6].x) < 0.05 and abs(results.left_hand_landmarks.landmark[3].y - results.left_hand_landmarks.landmark[6].y) < 0.05 and abs(results.right_hand_landmarks.landmark[3].x - results.right_hand_landmarks.landmark[6].x) < 0.05 and abs(results.right_hand_landmarks.landmark[3].y - results.right_hand_landmarks.landmark[6].y) < 0.05:
             
                 right_left_count_1+=1
                 if right_left_count_1 == effect.get(cv2.CAP_PROP_FRAME_COUNT):
@@ -68,6 +71,8 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 ok, eff = another_3.read()
                 if not ok:
                     continue
+                ratio = float(right_left_count_1)/effect.get(cv2.CAP_PROP_FRAME_COUNT)
+                bg = cv2.resize(bg, (int(bg.shape[1]*(1+ratio*3)), int(bg.shape[0]*(1+ratio*3))))
                 w, h, c = bg.shape
                 e_w, e_h, e_c = eff.shape
                 eff = cv2.resize(eff, (fr_h, fr_w))
@@ -79,7 +84,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 translation_matrix_1 = np.array([ [1, 0, int(x-h/2)] ,[0, 1, int(y-w/2)]], dtype=np.float32)
                 translated_bg_1 = cv2.warpAffine(src=bg, M=translation_matrix_1, dsize=(fr_h, fr_w))
                 translation_matrix_2 = np.array([ [1, 0, int(z-h/2)] ,[0, 1, int(t-w/2)]], dtype=np.float32)
-                translated_bg_2 = cv2.warpAffine(src=bg, M=translation_matrix_2, dsize=(fr_h, fr_w))
+                translated_bg_2 = cv2.warpAffine(src=cv2.flip(bg, 1), M=translation_matrix_2, dsize=(fr_h, fr_w))
                 gif_1 = cv2.cvtColor(translated_bg_1, cv2.COLOR_BGR2GRAY)
                 gif_2 = cv2.cvtColor(translated_bg_2, cv2.COLOR_BGR2GRAY)
                 mask = results.segmentation_mask.copy()
@@ -92,16 +97,17 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 cv2.drawContours(mask, contours, -1, (100), 10)
                 frame[mask==100] = blur[mask==100]
                 gif_1[gif_1==0] = 255
-                frame[gif_1<255] = cv2.addWeighted(frame, alpha, translated_bg_1, 1 - alpha, 0)[gif_1<255]
+                frame[gif_1<255] = cv2.addWeighted(frame, alpha + ratio, translated_bg_1, 1 - alpha - ratio, 0)[gif_1<255]
                 gif_2[gif_2==0] = 255
-                frame[gif_2<255] = cv2.addWeighted(frame, alpha, translated_bg_2, 1 - alpha, 0)[gif_2<255]
+                frame[gif_2<255] = cv2.addWeighted(frame, alpha + ratio, translated_bg_2, 1 - alpha - ratio, 0)[gif_2<255]
 
                 # mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
                 # mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
         
         if results.left_hand_landmarks and results.right_hand_landmarks is None:
-            if abs(results.left_hand_landmarks.landmark[3].x - results.left_hand_landmarks.landmark[6].x) < 0.03 and abs(results.left_hand_landmarks.landmark[3].y - results.left_hand_landmarks.landmark[6].y) < 0.03:
-                # alpha = np.multiply(alpha, 1.0/255)
+            right_count_1 = 0
+            right_left_count_1 = 0
+            if abs(results.left_hand_landmarks.landmark[3].x - results.left_hand_landmarks.landmark[6].x) < 0.05 and abs(results.left_hand_landmarks.landmark[3].y - results.left_hand_landmarks.landmark[6].y) < 0.05:
                 left_count_1+=1
                 if left_count_1 == effect.get(cv2.CAP_PROP_FRAME_COUNT):
                     effect.set(cv2.CAP_PROP_POS_FRAMES, 0)
@@ -110,19 +116,19 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 if left_count_2 == another_1.get(cv2.CAP_PROP_FRAME_COUNT):
                     another_1.set(cv2.CAP_PROP_POS_FRAMES, 0)
                     left_count_2 = 0
-
+                
                 okay, bg = effect.read()
                 if not okay:
                     continue
                 _, eff = another_1.read()
-                
+                ratio = float(left_count_1)/effect.get(cv2.CAP_PROP_FRAME_COUNT)
+                bg = cv2.resize(bg, (int(bg.shape[1]*(1+ratio*3)), int(bg.shape[0]*(1+ratio*3))))
                 w, h, c = bg.shape
                 e_w, e_h, e_c = eff.shape
                 eff = cv2.resize(eff, (fr_h, fr_w))
                 x = int(results.left_hand_landmarks.landmark[3].x *fr_h)
                 y = int(results.left_hand_landmarks.landmark[3].y *fr_w)
 
-                # print(results.left_hand_landmarks.landmark[3].z)
                 translation_matrix = np.array([ [1, 0, int(x-h/2)] ,[0, 1, int(y-w/2)]], dtype=np.float32)
                 translated_bg = cv2.warpAffine(src=bg, M=translation_matrix, dsize=(fr_h, fr_w))
                 gif = cv2.cvtColor(translated_bg, cv2.COLOR_BGR2GRAY)
@@ -136,15 +142,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 cv2.drawContours(mask, contours, -1, (100), 10)
                 frame[mask==100] = blur[mask==100]
                 gif[gif==0] = 255
-                frame[gif<255] = cv2.addWeighted(frame, alpha, translated_bg, 1 - alpha, 0)[gif<255]
+                frame[gif<255] = cv2.addWeighted(frame, alpha + ratio, translated_bg, 1 - alpha - ratio, 0)[gif<255]
                     
             # mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
             
         if results.right_hand_landmarks and results.left_hand_landmarks is None:
-            # thumb = [results.left_hand_landmarks.landmark[1].x - results.left_hand_landmarks.landmark[4].x, results.left_hand_landmarks.landmark[1].y - results.left_hand_landmarks.landmark[4].y]
-            # index = [results.left_hand_landmarks.landmark[5].x - results.left_hand_landmarks.landmark[8].x, results.left_hand_landmarks.landmark[5].y - results.left_hand_landmarks.landmark[8].y]
-            if abs(results.right_hand_landmarks.landmark[3].x - results.right_hand_landmarks.landmark[6].x) < 0.03 and abs(results.right_hand_landmarks.landmark[3].y - results.right_hand_landmarks.landmark[6].y) < 0.03:
-                # alpha = np.multiply(alpha, 1.0/255)
+            left_count_1 = 0
+            right_left_count_1 = 0
+            if abs(results.right_hand_landmarks.landmark[3].x - results.right_hand_landmarks.landmark[6].x) < 0.05 and abs(results.right_hand_landmarks.landmark[3].y - results.right_hand_landmarks.landmark[6].y) < 0.05:
                 right_count_1+=1
                 if right_count_1 == effect.get(cv2.CAP_PROP_FRAME_COUNT):
                     effect.set(cv2.CAP_PROP_POS_FRAMES, 0)
@@ -157,7 +162,8 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 if not okay:
                     continue
                 _, eff = another_2.read()
-            
+                ratio = float(right_count_1)/effect.get(cv2.CAP_PROP_FRAME_COUNT)
+                bg = cv2.resize(bg, (int(bg.shape[1]*(1+ratio*3)), int(bg.shape[0]*(1+ratio*3))))
                 w, h, c = bg.shape
                 e_w, e_h, e_c = eff.shape
 
@@ -170,7 +176,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 mask = results.segmentation_mask.copy()
                 mask[mask <= 0.75] = 0
                 mask = (mask*255).astype('uint8')
-                edges = cv2.Canny(mask,200,200)
+                edges = cv2.Canny(mask,100,200)
                 contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                 blend = frame[int(y-w/2):int(y+w/2), int(x-h/2):int(x+h/2), :]
                 frame[mask != 0] = frame[mask != 0] * gamma + eff[mask != 0] * (1 - gamma)
@@ -178,7 +184,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 cv2.drawContours(mask, contours, -1, (100), 10)
                 frame[mask==100] = blur[mask==100]
                 gif[gif==0] = 255
-                frame[gif<255] = cv2.addWeighted(frame, alpha, translated_bg, 1 - alpha, 0)[gif<255]
+                frame[gif<255] = cv2.addWeighted(frame, alpha + ratio, translated_bg, 1 - alpha - ratio, 0)[gif<255]
 
             # mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
             
@@ -187,7 +193,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         fps = 1/(end - start)
 
         # display FPS on the frame
-        cv2.putText(frame, str(f'FPS: {int(fps)}'), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+        # cv2.putText(frame, str(f'FPS: {int(fps)}'), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
 
         #resize window screen
         cv2.namedWindow('Webcam Feed', cv2.WINDOW_NORMAL)
@@ -195,7 +201,8 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         # Display the resulting frame
         cv2.imshow('Webcam Feed', frame)
-
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames.append(frame)
         # the 'q' button is set as the
         # quitting button you may use any
         # desired button of your choice
@@ -206,3 +213,4 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 cap.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
+io.mimsave('result.mp4', frames)
